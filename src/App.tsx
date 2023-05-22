fb8cde5f130c1cf10a2ff5a4f868088187787d54
raushan kuamr <sraushan890@gmail.com>
2023-05-22 21:54:56 +0530

    creating from components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNotes } from "./Components/NewNotes";
 function App() {
   return (
-    // <Container className="my-4">
+    <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<NewNotes/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    // </Container>
+     </Container>
   );
 }
 
---
+++ src/Components/NewNotes.tsx
@@ -0,0 +1,12 @@
+import { NoteForm } from "./NoteForm"
+
+
+export const NewNotes = () => {
+    
+    return (
+        <>
+            <h1 className="mb-4">New Notes</h1>
+            <NoteForm/>
+        </>
+    )
+}
