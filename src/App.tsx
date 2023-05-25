6a4cd0edca8419aa31bee61eb183a061f197a30e
raushan kuamr <sraushan890@gmail.com>
2023-05-25 21:44:18 +0530

    form note adding multi functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useLocalStorage } from "./useLocalStorage";
 import { NewNotes } from "./Components/NewNotes";
+
+export type Note = {
+  id:string
+} & NoteData
+
+export type RawNote = {
+  id: string
+};
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+};
+export type Tag = {
+  id: string
+  label:string
+}
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
   return (
     <Container className="my-4">
       <Routes>
