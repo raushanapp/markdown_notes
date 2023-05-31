4b6904de956eec4af5e50b68d562c17784554de6
raushan kuamr <sraushan890@gmail.com>
2023-06-01 02:55:21 +0530

    note card and newlist components

---
+++ src/Components/NoteCard.tsx
@@ -0,0 +1,10 @@
+
+export const NoteCard = () => {
+    
+
+    return (
+        <>
+            <h1>Note card</h1>
+        </>
+    )
+}
--- src/Components/NoteList.tsx
+++ src/Components/NoteList.tsx
@@ -1,7 +1,96 @@
+import {
+  Col,
+  Row,
+  Badge,
+  Button,
+  Card,
+  Form,
+  Modal,
+  Stack,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Note, Tag } from "../App";
+import { useState, useMemo } from "react";
+import { NoteCard } from "./NoteCard";
 
-export const NoteList = () => {
-    
-    return (
-        <h1>Note List</h1>
-    )
-}
+export type NoteListProps = {
+    availableTags: Tag[]
+    notes:Note[]
+};
+export const NoteList = ({ availableTags,notes }: NoteListProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+
+    const filteredNotes = useMemo(() => {
+        return notes.filter((note) => {
+            return (title===""||note.title.toLowerCase().includes(title.toLocaleLowerCase()))
+        })
+    },[title,selectedTags,notes])
+  return (
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>Notes</Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                value={selectedTags.map((tag) => {
+                  return {
+                    label: tag.label,
+                    value: tag.id,
+                  };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+          </Form>
+          <Row
+              xs={1}
+              sm={2}
+              lg={3}
+              xl={4}
+              className="g-3"
+          >
+              {filteredNotes.map((note) => (
+                  <Col key={note.id}>
+                      <NoteCard/>
+                  </Col>
+              ))}
+          </Row>
+    </>
+  );
+};
